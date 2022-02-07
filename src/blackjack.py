9a965fc11b0b033706393e8840e0615d956064ac
Louis Fernandes <fern.louis@gmail.com>
2022-02-07 00:18:59 -0600

    Added in a bit of a wagering framework and switched to multiple hands per player. Next up is to deal with the dealer hand and to determine who wins and who loses.

--- src/blackjack.py
+++ src/blackjack.py
@@ -8,6 +8,7 @@ import random
 import pandas as pd
 
 STANDARD_DECK = ["A", "10", "10", "10", "10", "9", "8", "7", "6", "5", "4", "3", "2"]*4 
+BLACKJACK = {"A","10"}
 
 def isInteger(s:str)->bool:
     # taken from stack overflow
@@ -50,59 +51,90 @@ class Game():
 
 
     """
-    def __init__(self, n_players:int = 1, deck=None):
+    def __init__(self, n_players:int = 1, deck=None, hands_per_player:int=2):
         self.dealer = Dealer()
-        self.n_players = n_players
+        self.players = [Player(n_hands=hands_per_player) for x in range(n_players)]
         self.hands = []
         self.final_hands = []
-        self.rule_location = "../data/basic_strategy_no_double_split.csv"
-        self.rule_set = self.get_rules(self.rule_location)
+        self.player_rule_location = "../data/basic_strategy_no_double_split.csv"
+        self.dealer_rule_location = "../data/simple_dealer_s17_stand.csv"
+        self.rule_set = self.get_rules(self.player_rule_location)
+        self.dealer_rule_set = self.get_rules(self.dealer_rule_location, idx="DH")
         if deck is None:
             self.deck = Deck().shuffle()
 
     def perform_round(self):
+        
+        # Players bet
+        for p in self.players:
+            p.wager()
+
         self.dealer.hole = self.deck.draw()
 
-        self.hands = [[self.deck.draw(), self.deck.draw()] for x in range(self.n_players)]
+        # Draw hands
+        for p in self.players:
+            p.deal_hands(self.deck)
 
         self.dealer.up = self.deck.draw()
 
         # Check for dealer blackjack
-        print(self.hands)
 
-        while self.hands:
-            hand = self.hands.pop()
+        dealer_has_blackjack = {self.dealer.hole, self.dealer.up}==BLACKJACK
+        if dealer_has_blackjack:
+            # If dealer has blackjack, just pass everyone's hands on to the final
+            # list of hands and we will evaluate later
+            # hack
+            # ignoring insurance for now
+            for p in self.players:
+                p.final_hands = p.hands
+            return None
 
-            # If you can split, and should split, do so and append two new hands
-            if len(set(hand))==1: # can split
-                hand_string = hand[0] + "-" + hand[1]
-                
-                action = self.rule_set.loc[hand_string, self.dealer.up]
-                
-                if action == "spl": # should split
-                    self.hands.append([hand[0], self.deck.draw()])
-                    self.hands.append([hand[1], self.deck.draw()])
-                    print("splitting, new stack of hands to deal:")
-                    print(self.hands)
-                    continue
-
-            # If you can't split, figure out what your hand is and take the next step
+        [print(p.hands) for p in self.players]
     
-            next_action = self.determine_hand_action(hand, self.dealer.up, self.rule_set)
-            while (next_action!="-") and (next_action!="dbs"):
-                if next_action=="B":
-                    # hand = ["BUSTED"]
-                    next_action = "-"
-                    continue
-                if next_action=="db":
-                    hand.append(self.deck.draw())
-                    next_action = "-"
-                    print("DOUBLED, FIGURE OUT HOW TO CHANGE THAT BET")
-                if next_action=="h" or next_action=="sr":
-                    hand.append(self.deck.draw())
-                next_action = self.determine_hand_action(hand, self.dealer.up, self.rule_set)
+        for p in self.players:
+            while p.hands:
+                hand = p.hands.pop()
 
-            self.final_hands.append(hand)
+                # If you can split, and should split, do so and append two new hands
+                if len(set(hand))==1: # can split
+                    hand_string = hand[0] + "-" + hand[1]
+                    
+                    # TODO: if we're splitting aces, only get one card apiece
+
+                    action = self.rule_set.loc[hand_string, self.dealer.up]
+                    
+                    if action == "spl": # should split
+                        p.hands.append([hand[0], self.deck.draw()])
+                        p.hands.append([hand[1], self.deck.draw()])
+                        print("splitting, new stack of hands to deal:")
+                        print(p.hands)
+                        continue
+
+                # If you can't split, figure out what your hand is and take the next step
+        
+                next_action = self.determine_hand_action(hand, self.dealer.up, self.rule_set)
+                while (next_action!="-") and (next_action!="dbs"):
+                    if next_action=="B":
+                        # hand = ["BUSTED"]
+                        next_action = "-"
+                        continue
+                    if next_action=="db":
+                        hand.append(self.deck.draw())
+                        next_action = "-"
+                        print("DOUBLED, FIGURE OUT HOW TO CHANGE THAT BET")
+                    if next_action=="h" or next_action=="sr":
+                        hand.append(self.deck.draw())
+                    next_action = self.determine_hand_action(hand, self.dealer.up, self.rule_set)
+
+                p.final_hands.append(hand)
+            
+        # Dealer has to play their hand
+
+        # Assess the final hands against the dealer hand
+        self.assess_hand_against_dealer()
+
+    def assess_hand_against_dealer(self):
+        return 1
     
     @staticmethod
     def determine_hand_action(hand, up, rule_set) -> str:
@@ -139,8 +171,8 @@ class Game():
         return None
 
     @classmethod
-    def get_rules(cls, path:str) -> pd.DataFrame:
-        return pd.read_csv(path, sep=";").set_index("PH")
+    def get_rules(cls, path:str, idx:str="PH") -> pd.DataFrame:
+        return pd.read_csv(path, sep=";").set_index(idx)
 
     @classmethod
     def figure_out_soft_hand(cls, hand, n_aces:int):
@@ -180,12 +212,10 @@ class Game():
 
 # class Agent
 class Agent():
-    def __init__(self, bank:int=100):
+    def __init__(self, bank:float=100):
         self.bank=bank
         self.hand=None
         self.hand_value = None # parsed value that corresponds to rule set
-
- 
 
 # class Dealer sup Agent
 class Dealer(Agent):
@@ -194,6 +224,32 @@ class Dealer(Agent):
         self.bank = 10E6
         self.up = None
         self.hole = None
+
+# class Player sup Agent
+class Player(Agent):
+    def __init__(self, bank:float=100, n_hands:int=1):
+        super().__init__(bank=bank)
+        self.n_hands=n_hands
+        self.hands=[]
+        self.final_hands=[]
+        self.wagers=[]
+        self.bet_size=5
+    
+    def update_bet_size(self, game) -> float:
+        """
+        Update the bet size depending on game state. 
+        Not implemented
+        """
+        raise NotImplementedError()
+
+    def deal_hands(self, deck):
+        self.hands = [[deck.draw(), deck.draw()] for n in range(self.n_hands)]
+
+    def wager(self):
+        self.wagers = [self.bet_size for n in range(self.n_hands)]
+        
+
+
 
 def main():
     deck = Deck()
@@ -205,7 +261,7 @@ def main():
     print("Dealer   UP {}   HOLE {}".format(game.dealer.up, game.dealer.hole))
     
     print("final hands")
-    print(game.final_hands)
+    [print(p.final_hands) for p in game.players]
 
 if __name__=="__main__":
     main()
